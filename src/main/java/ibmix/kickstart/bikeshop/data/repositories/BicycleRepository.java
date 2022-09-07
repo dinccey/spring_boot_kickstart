a5136b15cfd8f3500bcb213537b5f4b1481c9500
vaslim <mislav.jancic@protonmail.com>
2022-09-07 10:58:26 +0200

    Feature 7 completed (get bicycles by brand)

--- src/main/java/ibmix/kickstart/bikeshop/data/repositories/BicycleRepository.java
+++ src/main/java/ibmix/kickstart/bikeshop/data/repositories/BicycleRepository.java
@@ -2,8 +2,13 @@ package ibmix.kickstart.bikeshop.data.repositories;
 
 import ibmix.kickstart.bikeshop.data.entities.Bicycle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BicycleRepository extends JpaRepository<Bicycle,Long> {
+    @Query("SELECT b FROM Bicycle b WHERE b.brand.name = ?1")
+    List<Bicycle> bicyclesByBrand(String brandName);
 }
