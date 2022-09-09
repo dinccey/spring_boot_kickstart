96e4740b97a918db3bf5291fff17952d50ecb125
vaslim <mislav.jancic@protonmail.com>
2022-09-09 14:24:20 +0200

    Feature/13 Optimize imports

--- src/main/java/ibmix/kickstart/bikeshop/BikeshopApplication.java
+++ src/main/java/ibmix/kickstart/bikeshop/BikeshopApplication.java
@@ -2,7 +2,6 @@ package ibmix.kickstart.bikeshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class BikeshopApplication {
--- src/main/java/ibmix/kickstart/bikeshop/config/SpringSecurityConfig.java
+++ src/main/java/ibmix/kickstart/bikeshop/config/SpringSecurityConfig.java
@@ -1,20 +1,11 @@
 package ibmix.kickstart.bikeshop.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
